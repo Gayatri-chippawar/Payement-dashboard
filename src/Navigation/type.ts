c118f4a252b501c763ec8a5dd27739733037d665
Gayatri Chippawar <gayatrichippawar@gmail.com>
2025-08-03 00:31:36 +0530

    Delete src directory

--- src/Navigation/type.ts
+++
@@ -1,7 +0,0 @@
-// यह फाइल आपके सभी नेविगेशन टाइप्स को एक जगह रखेगी।
-export type RootStackParamList = {
-  DashboardScreen: undefined;
-  AddPaymentScreen: undefined;
-  TransactionListScreen: undefined;
-  TransactionDetails: { transactionId: string };
-};
